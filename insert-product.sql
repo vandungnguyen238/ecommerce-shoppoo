1099fef5a43b5b38a50037f2c8de15698efa69ac
vandungnguyen238 <aodaivietnam000@gmail.com>
2021-03-08 10:33:00 +0700

    add file insert Product.sql

---
+++ insert-product.sql
@@ -0,0 +1,277 @@
+use ecommerceshopoo;
+
+insert into manufacture values (1,"Apple"),(2,"Samsung"),(3,"Huawei"),(4,"LG"),(5,"Alienware"),(6,"Msi");
+
+insert into types values (1,"Cellphone"),(2,"Laptop"),(3,"Tablet");
+
+insert into products values 
+(1,
+"6.1 inches or 6.7  inches
+Super Retina XDR display3
+5G cellular4
+A14 Bionic chip
+Pro camera system
+(Ultra Wide, Wide, Telephoto)
+LiDAR Scanner for Night mode portraits and next‑level AR
+",
+"Iphone12 pro",100,999,"2021-02-23","3 years",1,1),
+
+(2,
+"5.4 inches or 6.1 inches
+Super Retina XDR display
+5G cellular
+A14 Bionic chip
+Advanced dual‑camera system
+(Ultra Wide, Wide)",
+"Iphone 12",100,699,"2021-02-23","3 years",1,1),
+
+(3,
+"6.1 inches
+Liquid Retina HD display
+4G LTE cellular
+A13 Bionic chip
+Dual-camera system
+(Ultra Wide, Wide)",
+"Iphone11",100,599,"2021-02-23","3 years",1,1),
+
+(4,
+"4.7 inches
+Retina HD display
+4G LTE cellular
+A13 Bionic chip
+Single-camera system
+(Wide)",
+"IphoneSE",100,399,"2021-02-23","3 years",1,1),
+
+(5,
+"6.58 inches FHD+ OLED touchscreen
+Kirin 990 5G SOC with high efficiency
+Leica Quad 50 MP / 40 MP / 12 MP / 2 MP main cameras",
+"Huawei P40 Pro",100,1199,"2021-02-23","3 years",3,1),
+
+(6,
+"8 inches FHD+ OLED foldable touchscreen
+Kirin 990 5G SOC with high efficiency
+Leica Quad 40 MP / 16 MP / 8 MP / 2 MP main cameras",
+"Huawei Mate Xs",100,2199,"2021-02-23","3 years",3,1),
+
+(7,
+"Versatile Multi-Screen Form Factor with Swivel Mode 
+6.8 inches OLED FullVision Main Display
+64MP Main, 13MP Ultra-Wide, 12MP Ultra-Wide Angle with Hexa Motion Sensors
+32MP Pop-up Selfie Camera 
+4K Video Recording 
+4,000mAh Battery",
+"LG WING 5G",100,1049,"2021-02-23","3 years",4,1),
+
+(8,
+"Ultimate multi-tasking with the LG Dual Screen™ (Sold separately) 
+6.8 inches OLED Display 
+8K Video Recording
+Triple Camera System: 64 MP Standard, 13 MP Ultra-Wide, Z-Camera 
+All-Day 5,000 mAh Battery 
+5G Connectivity",
+"LG V60 ThinQ 5G",100,899,"2021-02-23","3 years",4,1),
+
+(9,
+"5.7 inches HD+ FullVision™ Display 
+13MP Autofocus Rear Camera 
+5MP Front-Facing Camera with LED flash
+Rear Key with Fingerprint Sensor 
+3,000 mAh Non-Removable Battery",
+"LG Xpression Plus 2",100,599,"2021-02-23","3 years",4,1),
+
+(10,
+"5nm Kirin 9000 5G SoC Chipset
+Ultra Vision Cine Camera and Selfie Camera
+Rear Camera:50 MP Ultra Vision Camera (Wide Angle, f/1.9 aperture) 20 MP Cine"
+,"Huawei Mate40 Pro",100,1499,"2021-02-23","3 years",3,1),
+
+(11,
+"6.8 inches edge Quad HD+ Dynamic AMOLED 2X
+Infinity-O Display (3200x1440)
+515 ppi
+HDR10+ certified
+120Hz refresh rate",
+"Galaxy S21 Ultra 5G",100,1199,"2021-02-23","3 years",2,1),
+
+(12,
+"6.7 inches flat FHD+ Dynamic AMOLED 2X
+Infinity-O Display (2400x1080)
+394 ppi
+HDR10+ certified
+120Hz refresh rate",
+"Galaxy S21+ 5G",100,999,"2021-02-23","3 years",2,1),
+
+(13,
+"6.9 inches edge Quad HD+ Dynamic AMOLED 2X Infinity-O Display (3088x1440)
+496 ppi
+HDR10+ certified
+120Hz refresh rate",
+"Galaxy Note20 Ultra 5G",100,1299,"2021-02-23","3 years",2,1),
+
+(14,
+"256 GB / 12 GB RAM 
+12 MP + 12 MP + 12 MP Triple lens Primary Camera, 10 MP Front Camera 
+7.6 inches 1768 x 2208 pixels 
+Display Qualcomm SM8250 Snapdragon 865+ (7 nm+) 
+Non-removable Li-Ion 4500 mAh battery",
+"Galaxy Z Fold2 5G",100,1999,"2021-02-23","3 years",2,1),
+
+(15,
+"Qualcomm SM8150 Snapdragon 855+ (7 nm)
+RAM: 8GB
+Storage: 256GB
+Screen Size: 6.7 inches
+Screen Resolution: 1080 x 2636 pixels
+Battery Capacity : 3300mAh
+Battery Type: Non-removable Li-Po battery",
+"Galaxy Z Flip",100,1299,"2021-02-23","3 years",2,1),
+
+(16,
+"10th Generation Intel® Core™ i7-10750H
+Windows 10 Home
+AMD Radeon™ RX 5500M 4GB GDDR6
+16GB DDR4 2666MHz
+256GB PCIe M.2 SSD
+display
+15.6-in. display",
+"Alienware m15 R3 Gaming",50,1549,"2021-02-23","3 years",5,2),
+
+(17,
+"10th Gen Intel® Core™ i7 10700
+Windows 10 Home
+AMD Radeon™ RX 5700M 8GB GDDR6
+16GB DDR4-2933MHz, 2x8G
+512GB NVMe M.2 PCIe SSD
+display
+17.3-in. display",
+"Alienware Area-51m R2 Gaming",50,2099,"2021-02-23","3 years",5,2),
+
+(18,
+"Windows 10 Pro
+Intel® Core™ i7-10875H 2.3GHz
+15.6 inches FHD Finger Touch panel, 60Hz 72% NTSC
+8G GDDR6 NVIDIA® GeForce RTX™ 2070 Max-Q
+16GB (8G*2) DDR4 2666MHz
+512GB NVMe SSD
+Intel® Ethernet Controller I225-V
+Intel Wi-Fi 6 AX201
+",
+"Creator 15 A10SFT-053",50,1899,"2021-02-23","3 years",6,2),
+
+(19,
+"Windows 10 
+Intel® Core™ i7-9750H 2.6GHz
+15.6 inches FHD 120Hz 3ms 94% NTSC color Anti-Glare, 100% sRGB
+6G GDDR6 NVIDIA GeForce® GTX1660Ti
+16GB (8G*2) DDR4 2666MHz
+256GB NVMe SSD
+1TB (7200RPM)
+",
+"",50,1099,"2021-02-23","3 years",6,2),
+
+(20,
+"Windows 10 Pro
+Intel® Core™ i5-10210U 1.8GHz
+14 inches THIN BEZEL Full HD IPS-Level 72% NTSC
+8GB (8G*1) DDR4 2666MHz
+512GB NVMe SSD   
+USB Dongle",
+"Modern 14 A10M-1029",50,824,"2021-02-23","3 years",6,2),
+
+(21,
+"Windows 10 
+Intel® Core™ i7-9750H 2.6GHz
+15.6 inches FHD, IPS-Level 144Hz 3ms 72% NTSC, 100% sRGB
+8G GDDR6 NVIDIA GeForce® RTX2070
+16GB (8G*2) DDR4 2666MHz
+256GB NVMe SSD
+1TB (7200RPM)",
+"GE63 Raider RGB-608",50,1399,"2021-02-23","3 years",6,2),
+
+(22,
+"13.3 inches Retina display
+Apple M1 chip
+Up to 16GB memory
+Up to 2TB storage
+Up to 18 hours battery life",
+"Macbook Air",50,999,"2021-02-23","3 years",1,2),
+
+(23,
+"13.3 inches Retina display
+Apple M1 chip
+Also available with Intel Core i5 or
+i7 processor
+Up to 16GB memory
+Up to 2TB storage
+Up to 20 hours battery life",
+"Macbook Pro 13",50,1299,"2021-02-23","3 years",1,2),
+
+(24,
+"16 inches Retina display
+Intel Core i7 or i9 processor
+Up to 64GB memory
+Up to 8TB storage
+Up to 11 hours battery life",
+"Macbook Pro 16",50,2399,"2021-02-23","3 years",1,2),
+
+(25,
+"12.9 inches and 11 inches
+Liquid Retina display with ProMotion
+A12Z Bionic chip
+Compatible with Magic Keyboard and Smart Keyboard Folio
+Compatible with Apple Pencil (2nd generation)",
+"Ipad Pro",50,799,"2021-02-23","3 years",1,3),
+
+(26,
+"10.9 inches
+Liquid Retina display
+A14 Bionic chip
+Compatible with Magic Keyboard and Smart Keyboard Folio
+Compatible with Apple Pencil (2nd generation)",
+"Ipad Air",50,599,"2021-02-23","3 years",1,3),
+
+(27,
+"10.2 inches
+Retina display
+A12 Bionic chip
+Compatible with Smart Keyboard
+Compatible with Apple Pencil (1st generation)",
+"Ipad",50,329,"2021-02-23","3 years",1,3),
+
+(28,
+"Android: Operating system
+Processor: 2.5 GHz
+Screen Size: 12.4 inches
+Camera: 8+ MP
+Battery: 7040 mAh",
+" Galaxy Tab S7+",50,699,"2021-02-23","3 years",2,3),
+
+(29,
+"Android: Operating system
+Processor: 1.7 GHz
+Screen Size: 10.4 inches
+Camera: 8+ MP
+Battery: 7040 mAh",
+"Galaxy Tab S6 Lite",50,399,"2021-02-23","3 years",2,3),
+
+(30,
+"Android: Operating system
+Processor: 2 GHz
+Screen Size: 10.4 inches
+Camera: 8+ MP
+Battery: 7040 mAh",
+"Galaxy Tab A7",50,499,"2021-02-23","3 years",2,3);
+
+
+
+insert into images(id,firstLink,products_fk) values 
+(1,"/resources/img/product-img/product-1.jpg",1),(2,"/resources/img/product-img/product-2.jpg",2),(3,"/resources/img/product-img/product-3.jpg",3),(4,"/resources/img/product-img/product-4.jpg",4),(5,"/resources/img/product-img/product-5.jpg",5),
+(6,"/resources/img/product-img/product-6.jpg",6),(7,"/resources/img/product-img/product-7.jpg",7),(8,"/resources/img/product-img/product-8.jpg",8),(9,"/resources/img/product-img/product-9.jpg",9),(10,"/resources/img/product-img/product-10.jpg",10),
+(11,"/resources/img/product-img/product-11.jpg",11),(12,"/resources/img/product-img/product-12.jpg",12),(13,"/resources/img/product-img/product-13.jpg",13),(14,"/resources/img/product-img/product-14.jpg",14),(15,"/resources/img/product-img/product-15.jpg",15),
+(16,"/resources/img/product-img/product-16.jpg",16),(17,"/resources/img/product-img/product-17.jpg",17),(18,"/resources/img/product-img/product-18.jpg",18),(19,"/resources/img/product-img/product-19.jpg",19),(20,"/resources/img/product-img/product-20.jpg",20),
+(21,"/resources/img/product-img/product-21.jpg",21),(22,"/resources/img/product-img/product-22.jpg",22),(23,"/resources/img/product-img/product-23.jpg",23),(24,"/resources/img/product-img/product-24.jpg",24),(25,"/resources/img/product-img/product-25.jpg",25),
+(26,"/resources/img/product-img/product-26.jpg",26),(27,"/resources/img/product-img/product-27.jpg",27),(28,"/resources/img/product-img/product-28.jpg",28),(29,"/resources/img/product-img/product-29.jpg",29),(30,"/resources/img/product-img/product-30.jpg",30);
+
+
